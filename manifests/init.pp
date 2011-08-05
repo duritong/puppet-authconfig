adad3fca08a384124793f50283845d0398524a4c
mh <mh@immerda.ch>
2011-08-05 12:51:27 +0200

    init of authconfig module

---
+++ manifests/init.pp
@@ -0,0 +1,19 @@
+class authconfig($algo='sha512') {
+
+  package{'authconfig': ensure => installed }
+
+  exec{'ensure_shadowed_passwd':
+    command => 'pwconf',
+    creates => '/etc/shadow',
+  }
+  exec{'ensure_shadowed_group':
+    command => 'grpconv',
+    creates => '/etc/gshadow',
+  }
+
+  exec{"ensure_${authconfig::algo}_authtconfig":
+    command => "authconfig --passalgo=${authconfig::algo} --update",
+    unless => "authconfig --test | grep -q 'password hashing algorithm is ${authconfig::algo}'",
+    require => [ Package['authconfig'], Exec['ensure_shadowed_passwd','ensure_shadowed_group'] ],
+  }
+}
